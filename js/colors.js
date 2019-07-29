0363ed09e07d87f85c20eee4f29191477c60b853
Ahmed Ramzy <46076630+AhmedRamzyy@users.noreply.github.com>
2019-07-29 21:37:22 +0200

    Initial commit

---
+++ js/colors.js
@@ -0,0 +1,25 @@
+
+function blue() {
+    document.getElementById("screen").style.backgroundColor = "#010059";
+}
+function green() {
+    document.getElementById("screen").style.backgroundColor = "#2b580c";
+}
+function red() {
+    document.getElementById("screen").style.backgroundColor = "#af0404";
+}
+function lightblue() {
+    document.getElementById("screen").style.backgroundColor = "#263238";
+}
+function yellow() {
+    document.getElementById("screen").style.backgroundColor = "#F8D800";
+}
+function moov() {
+    document.getElementById("screen").style.backgroundColor = "#7367F0";
+}
+function lightgreen() {
+    document.getElementById("screen").style.backgroundColor = "#32CCBC";
+}
+function darkbrown() {
+    document.getElementById("screen").style.backgroundColor = "#360940";
+}
